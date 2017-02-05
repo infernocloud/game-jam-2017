7fe5ae862323655a4da14aef0b625a0941b53d2d
blunket <blerrnk@gmail.com>
2017-02-04 22:04:20 -0600

    game logic

--- client/cardDefinitions.js
+++ client/cardDefinitions.js
@@ -1,167 +1,167 @@
 var cardDefs = [
     {
-        "flavortext": "",
+        "flavortext": "Guns Come With Instruction Manuals",
         "military": -2,
         "education": 1,
         "agriculture": 0,
         "business": -1
     },
     {
-        "flavortext": "",
+        "flavortext": "All Babies Get Pacifist Pacifiers At Birth",
         "military": -2,
         "education": 1,
         "agriculture": -1,
         "business": 0
     },
     {
-        "flavortext": "",
+        "flavortext": "No War on Tuesdays",
         "military": -2,
         "education": 0,
         "agriculture": 1,
         "business": -1
     },
     {
-        "flavortext": "",
+        "flavortext": "Tanks Must Have Wheels, Not Treads",
         "military": -2,
         "education": 0,
         "agriculture": -1,
         "business": 1
     },
     {
-        "flavortext": "",
+        "flavortext": "Snack Food Sandwiches",
         "military": -2,
         "education": -1,
         "agriculture": 1,
         "business": 0
     },
     {
-        "flavortext": "",
+        "flavortext": "18 Yr Olds Must Do 2 Yrs Civil Service",
         "military": -2,
         "education": -1,
         "agriculture": 0,
         "business": 1
     },
     {
-        "flavortext": "",
+        "flavortext": "Slaughterhouse Five Banned",
         "military": 1,
         "education": -2,
         "agriculture": -1,
         "business": 0
     },
     {
-        "flavortext": "",
+        "flavortext": "All Country Songs Must Reference \"Freedumb\"",
         "military": 1,
         "education": -2,
         "agriculture": 0,
         "business": -1
     },
     {
-        "flavortext": "",
+        "flavortext": "All Buildings Capped at 6 Stories",
         "military": 1,
         "education": 0,
         "agriculture": -1,
         "business": -2
     },
     {
-        "flavortext": "",
+        "flavortext": "1/4 of Rural Land Reserved for Training Camps",
         "military": 1,
         "education": 0,
         "agriculture": -2,
         "business": -1
     },
     {
-        "flavortext": "",
+        "flavortext": "All Military Equipment Must Be Manufactured By Veterans",
         "military": 1,
         "education": -1,
         "agriculture": 0,
         "business": -2
     },
     {
-        "flavortext": "",
+        "flavortext": "All Ag Research Must Have Military Justification",
         "military": 1,
         "education": -1,
         "agriculture": -2,
         "business": 0
     },
     {
-        "flavortext": "",
+        "flavortext": "Fridays Are French Funyun Soup School Lunch Days",
         "military": 0,
         "education": -2,
         "agriculture": 1,
         "business": -1
     },
     {
-        "flavortext": "",
+        "flavortext": "Minimum Toxic Waste Per Gallon of Water",
         "military": 0,
         "education": -2,
         "agriculture": -1,
         "business": 1
     },
     {
-        "flavortext": "",
+        "flavortext": "Corn Plants Also Produce Tiny Dictionaries",
         "military": 0,
         "education": 1,
         "agriculture": -2,
         "business": -1
     },
     {
-        "flavortext": "",
+        "flavortext": "All Legal Businesses are State-Owned Corporations",
         "military": 0,
         "education": 1,
         "agriculture": -1,
         "business": -2
     },
     {
-        "flavortext": "",
+        "flavortext": "Aerosol Cans To Undergo \"Retro CFC\" Throwback",
         "military": 0,
         "education": -1,
         "agriculture": -2,
         "business": 1
     },
     {
-        "flavortext": "",
+        "flavortext": "Each Corporate Board of Directors Must Include One Goat",
         "military": 0,
         "education": -1,
         "agriculture": 1,
         "business": -2
     },
     {
-        "flavortext": "",
+        "flavortext": "GI Bill Cancelled And Replaced With Consultants",
         "military": -1,
         "education": -2,
         "agriculture": 0,
         "business": 1
     },
     {
-        "flavortext": "",
+        "flavortext": "GI Bill Cancelled And Replaced With Cheeze Whiz",
         "military": -1,
         "education": -2,
         "agriculture": 1,
         "business": 0
     },
     {
-        "flavortext": "",
+        "flavortext": "College Must Be Eight Years Long",
         "military": -1,
         "education": 1,
         "agriculture": 0,
         "business": -2
     },
     {
-        "flavortext": "",
+        "flavortext": "Schools To Grow Fresh Vegetables on Their Roofs",
         "military": -1,
         "education": 1,
         "agriculture": -2,
         "business": 0
     },
     {
-        "flavortext": "",
+        "flavortext": "Declaration That The F-35 Causes Global Warming",
         "military": -1,
         "education": 0,
         "agriculture": 1,
         "business": -2
     },
     {
-        "flavortext": "",
+        "flavortext": "Every 20th Broiler Chicken Shall Be Weaponized",
         "military": -1,
         "education": 0,
         "agriculture": -2,
